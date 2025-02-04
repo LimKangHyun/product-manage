d425d15dbf5965a6df1394ec1dd6bbdf4ff84428
limkanghyun <limkh06910691@gmail.com>
2025-02-04 23:04:27 +0900

    feat: ItemController 추가

---
+++ src/main/java/hello/product_manage/web/item/ItemController.java
@@ -0,0 +1,76 @@
+package hello.product_manage.web.item;
+
+import hello.product_manage.domain.Item;
+import hello.product_manage.domain.ItemRepository;
+import hello.product_manage.web.item.form.ItemSaveForm;
+import hello.product_manage.web.item.form.ItemUpdateForm;
+import jakarta.validation.Valid;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@RequestMapping("/items")
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "items/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "items/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "items/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItem(@Valid @ModelAttribute("item") ItemSaveForm form, RedirectAttributes redirectAttributes) {
+        Item item = new Item();
+
+        item.setItemName(form.getItemName());
+        item.setPrice(form.getPrice());
+        item.setQuantity(form.getQuantity());
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        return "redirect:/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "items/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute("item") ItemUpdateForm form) {
+
+        Item itemParam = new Item();
+        itemParam.setItemName(form.getItemName());
+        itemParam.setPrice(form.getPrice());
+        itemParam.setQuantity(form.getQuantity());
+
+        itemRepository.update(itemId, itemParam);
+
+        return "redirect:/items/{itemId}";
+    }
+}
