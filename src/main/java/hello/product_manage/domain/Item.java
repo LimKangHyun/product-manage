40c91f8abb2eb1bb7f3b4a10ca24864726dfd246
limkanghyun <limkh06910691@gmail.com>
2025-02-04 23:16:47 +0900

    feat: TestInitData 수정 및 로그인/회원가입 form.html 추가

--- src/main/java/hello/product_manage/domain/Item.java
+++ src/main/java/hello/product_manage/domain/Item.java
@@ -12,7 +12,7 @@ public class Item {
     public Item() {
     }
 
-    public Item(String name, Integer price, Integer quantity) {
+    public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
         this.price = price;
         this.quantity = quantity;
