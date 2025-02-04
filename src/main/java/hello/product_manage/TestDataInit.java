49e6aec0b852273210ea410ed032ab73d555e383
limkanghyun <limkh06910691@gmail.com>
2025-02-04 23:16:57 +0900

    feat: TestInitData 수정 및 로그인/회원가입 form.html 추가

--- src/main/java/hello/product_manage/TestDataInit.java
+++ src/main/java/hello/product_manage/TestDataInit.java
@@ -1,4 +1,4 @@
-package hello;
+package hello.product_manage;
 
 import hello.product_manage.domain.Item;
 import hello.product_manage.domain.ItemRepository;
@@ -8,12 +8,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class TestInitData {
+public class TestDataInit {
     private final ItemRepository itemRepository;
 
     @PostConstruct
     public void init() {
-        itemRepository.save(new Item("아이폰 16 Pro", 30000, 100));
-        itemRepository.save(new Item("갤럭시 S25 Ultra", 30000, 100));
+        itemRepository.save(new Item("아이폰 16 Pro", 1400000, 100));
+        itemRepository.save(new Item("갤럭시 S25 Ultra", 1250000, 100));
     }
 }
