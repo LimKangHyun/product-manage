dc9f924246d53980fa0a6e212810adcbc1d904b9
limkanghyun <limkh06910691@gmail.com>
2025-02-05 16:56:16 +0900

    feat: 멤버 TestInitData 추가

--- src/main/java/hello/product_manage/TestDataInit.java
+++ src/main/java/hello/product_manage/TestDataInit.java
@@ -1,7 +1,9 @@
 package hello.product_manage;
 
-import hello.product_manage.domain.Item;
-import hello.product_manage.domain.ItemRepository;
+import hello.product_manage.domain.item.Item;
+import hello.product_manage.domain.item.ItemRepository;
+import hello.product_manage.domain.member.Member;
+import hello.product_manage.domain.member.MemberRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -10,10 +12,17 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class TestDataInit {
     private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
 
     @PostConstruct
     public void init() {
         itemRepository.save(new Item("아이폰 16 Pro", 1400000, 100));
         itemRepository.save(new Item("갤럭시 S25 Ultra", 1250000, 100));
+
+        Member member = new Member();
+        member.setLoginId("test");
+        member.setPassword("test!");
+        member.setName("테스트용");
+        memberRepository.save(member);
     }
 }
