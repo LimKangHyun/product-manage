e030e20d0c1cf405d4248ded31c5394ce5132b03
limkanghyun <limkh06910691@gmail.com>
2025-02-11 22:40:52 +0900

    feat: README 수정

--- src/main/java/hello/product_manage/domain/item/Item.java
+++ src/main/java/hello/product_manage/domain/item/Item.java
@@ -1,14 +1,25 @@
 package hello.product_manage.domain.item;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 import java.io.Serializable;
 
+@Entity
 @Data
 public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String itemName;
+
     private Integer price;
+
     private Integer quantity;
 
     public Item() {
--- src/main/java/hello/product_manage/domain/member/Member.java
+++ src/main/java/hello/product_manage/domain/member/Member.java
@@ -1,23 +1,29 @@
 package hello.product_manage.domain.member;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 import java.io.Serializable;
 
+@Entity
 @Data
 public class Member implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-
+    @Id
     private Long id;
 
+    @Column(nullable = false, unique = true)
     @NotEmpty
     private String loginId;
 
+    @Column(nullable = false)
     @NotEmpty
     private String password;
 
+    @Column(nullable = false)
     @NotEmpty
     private String name;
 }
